57f75560e21526f616a12f4b28a7a602b260ffe5
weizenliu <t105360725@ntut.org.tw>
2018-02-28 20:29:27 +0800

    backup

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
 exports.test = (name) => {
 
-  var firebase = require("firebase")
+    var firebase = require("firebase")
 
-  var config = {
-    apiKey: "AIzaSyAPQCtYByX43Jejr5l1f5uv6-kHIGU7sJc",
-    authDomain: "care-8e4c7.firebaseapp.com",
-    databaseURL: "https://care-8e4c7.firebaseio.com",
-    projectId: "care-8e4c7",
-    storageBucket: "care-8e4c7.appspot.com",
-    messagingSenderId: "511100821138"
-  };
-  firebase.initializeApp(config);
-  var db = firebase.database();
-  
-  ref = db.ref("/");
+    var config = {
+        apiKey: "AIzaSyAPQCtYByX43Jejr5l1f5uv6-kHIGU7sJc",
+        authDomain: "care-8e4c7.firebaseapp.com",
+        databaseURL: "https://care-8e4c7.firebaseio.com",
+        projectId: "care-8e4c7",
+        storageBucket: "care-8e4c7.appspot.com",
+        messagingSenderId: "511100821138"
+    };
+    firebase.initializeApp(config);
+    var db = firebase.database();
 
-//
+    var ref = db.ref("/");
 
-var openalpr = require ("node-openalpr");
+    var openalpr = require ("node-openalpr");
 
-var lp_result = ""
+    function identify (path) {
+        openalpr.IdentifyLicense (path, function (error, output) {
+            var results = output.results;
 
-function identify (path) {
-  openalpr.IdentifyLicense (path, function (error, output) {
-    var results = output.results;
-    console.log (output.processing_time_ms +" "+ ((results.length > 0) ? results[0].plate : "No results"));
-    
-    let current_time_stamp = new Date().getTime()
+            if (results.length > 0) {
+                console.log("Processing time: " + output.processing_time_ms + "Number Plate: " + results[0].plate)
+                let current_time_stamp = new Date().getTime()
 
-    var value = {
-     number: results[0].plate,
-     time: parseInt(current_time_stamp / 1000)
-   }
-   
-   console.log(value)
-   ref.push(value);
-  // if (id == 349) {
-           //  console.log (openalpr.Stop ());
-        // }
-      });
-}
+                var value = {
+                    number: results[0].plate,
+                    time: parseInt(current_time_stamp / 1000)
+                }
+                ref.push(value);
+            }
+            else {
+                console.log ("No results");
+            }
 
-openalpr.Start ();
-openalpr.GetVersion ();
+        });
+    }
 
-identify ("lps/" + name)
-
-// for (var i = 0; i < 350; i++) {
-//     identify (i, "lp.jpeg");
-// }
-
-// ref.push(value);
-var ref = db.ref("/");
-ref.once("value", function(snapshot) {
- console.log('========database files========')
- console.log(snapshot.val());
-});
-
+    openalpr.Start ();
+    openalpr.GetVersion ();
+    identify ("lps/" + name)
 }
--- watch.js
+++ watch.js
@@ -1,20 +1,12 @@
 'use strict'
 const fire_alpr = require('./index.js')
-const path = require('path');
-const chokidar = require('chokidar');
-const watcher = chokidar.watch(path.join(__dirname, './lps/*.*'));
+const path = require('path')
+const chokidar = require('chokidar')
+const watcher = chokidar.watch(path.join(__dirname, './lps/*.*'))
 watcher.on('ready', () => {
-    watcher.on('change', (path) => {
-        console.log(path);
-    });
     watcher.on('add', (path) => {
-        // console.log(path);
         var str = path;
-		var res = str.split("/");
-		// console.log(res[5])
+		var res = str.split("/")
         fire_alpr.test(res[5])
-    });
-    watcher.on('unlink', (path) => {
-        console.log(path);
-    });
-});
+    })
+})
