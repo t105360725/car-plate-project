93091d368fc2bd284a160c9d60e1cd2d85c18dad
weizenliu <t105360725@ntut.org.tw>
2018-03-01 10:22:35 +0800

    march 1

--- git.sh
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m "$(date -u)"
-git push -u origin master
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
-// const fire_alpr = require('./index.js')
+
 const path = require('path')
 const chokidar = require('chokidar')
 const watcher = chokidar.watch(path.join(__dirname, './lps/*.*'))
 var openalpr = require ("node-openalpr")
-// 
+
+// Connect to firebase database 
 var firebase = require("firebase")
 
 var config = {
@@ -17,24 +18,24 @@ var config = {
 };
 firebase.initializeApp(config);
 var db = firebase.database();
-
 var ref = db.ref("/");
 
+// Start openalpr
+openalpr.Start ()
+openalpr.GetVersion ()
+
+// Watch new photos
 watcher.on('ready', () => {
     watcher.on('add', (path) => {
         var str = path;
 		var res = str.split("/")
         // ref.push(fire_alpr.test(res[5]))
         var path = "lps/" + res[5]
-        openalpr.Start ();
-    	openalpr.GetVersion ();
     	openalpr.IdentifyLicense (path, function (error, output) {
 	        var results = output.results;
-
 	        if (results.length > 0) {
 	            console.log("Processing time: " + output.processing_time_ms + " Number Plate: " + results[0].plate)
 	            let current_time_stamp = new Date().getTime()
-
 	            res.number = results[0].plate
 	            res.time = parseInt(current_time_stamp / 1000)
 	            ref.push(res)
@@ -43,7 +44,6 @@ watcher.on('ready', () => {
 	        else {
 	            console.log ("No results");
 	        }
-
     	})
     })
 })
