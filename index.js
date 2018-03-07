e72a9bc4819d6942909e334667ef6998f54c1deb
weizenliu <t105360725@ntut.org.tw>
2018-03-07 16:55:16 +0800

    fix delay

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 'use strict'
 
-// Shotting 
-const exec = require('child_process').exec;
-var shots = () => exec('sh shot.sh');
-
 const path = require('path')
 const chokidar = require('chokidar')
-const watcher = chokidar.watch(path.join(__dirname, './lps/*.*'))
+const watcher = chokidar.watch(path.join(__dirname, './lps/*.*'), {
+	awaitWriteFinish: {
+		stabilityThreshold: 2000,
+		pollInterval: 100
+	}})
 var openalpr = require ("node-openalpr")
 
 // Connect to firebase database 
 var firebase = require("firebase")
 
 var config = {
-    apiKey: "AIzaSyAPQCtYByX43Jejr5l1f5uv6-kHIGU7sJc",
-    authDomain: "care-8e4c7.firebaseapp.com",
-    databaseURL: "https://care-8e4c7.firebaseio.com",
-    projectId: "care-8e4c7",
-    storageBucket: "care-8e4c7.appspot.com",
-    messagingSenderId: "511100821138"
+	apiKey: "AIzaSyAPQCtYByX43Jejr5l1f5uv6-kHIGU7sJc",
+	authDomain: "care-8e4c7.firebaseapp.com",
+	databaseURL: "https://care-8e4c7.firebaseio.com",
+	projectId: "care-8e4c7",
+	storageBucket: "care-8e4c7.appspot.com",
+	messagingSenderId: "511100821138"
 };
 firebase.initializeApp(config);
 var db = firebase.database();
 var ref = db.ref("/");
-
-// Shotting
-shots()
 
 // Start openalpr
 openalpr.Start ()
@@ -33,24 +30,23 @@ openalpr.GetVersion ()
 
 // Watch new photos
 watcher.on('ready', () => {
-    watcher.on('add', (path) => {
-        var str = path;
+	watcher.on('add', (path) => {
+		var str = path;
 		var res = str.split("/")
-        // ref.push(fire_alpr.test(res[5]))
-        var path = "lps/" + res[5]
-    	openalpr.IdentifyLicense (path, function (error, output) {
-	        var results = output.results;
-	        if (results.length > 0) {
-	            console.log("Processing time: " + output.processing_time_ms + " Number Plate: " + results[0].plate)
-	            let current_time_stamp = new Date().getTime()
-	            res.number = results[0].plate
-	            res.time = parseInt(current_time_stamp / 1000)
-	            ref.push(res)
-	            console.log("Upload success!")
-	        }
-	        else {
-	            console.log ("No results");
-	        }
-    	})
-    })
+		var path = "lps/" + res[5]
+		openalpr.IdentifyLicense (path, function (error, output) {
+			var results = output.results;
+			if (results.length > 0) {
+				console.log("Processing time: " + output.processing_time_ms + " Number Plate: " + results[0].plate)
+				let current_time_stamp = new Date().getTime()
+				res.number = results[0].plate
+				res.time = parseInt(current_time_stamp / 1000)
+				ref.push(res)
+				console.log("Upload success!")
+			}
+			else {
+				console.log ("No results");
+			}
+		})
+	})
 })
--- shot.sh
+++ shot.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 i=1
+j=400
 URL="http://192.168.0.104:8888"
 # GOOGLE="www.google.com"
-while [[ $i<5 ]]; do
-	response=$(curl --write-out %{http_code} --silent --output /dev/null $URL)
-	if [[ response != 404 ]]
-	then
+response=$(curl --write-out %{http_code} --silent --output /dev/null $URL)
+while :
+do
+	if [[ $response -le $j ]]; then
 		# firefox --screenshot $i.jpg http://192.168.0.104:8888 --window-size=550,900
-		firefox --screenshot $i.jpg $URL --window-size=550,900
+		firefox --screenshot ./lps/$i.png $URL
 		echo "Shot $i times"
 		i=$((i+1))
-		# sleep 1
+		# sleep 1	
 	fi
 done
